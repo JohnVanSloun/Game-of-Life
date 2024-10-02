27ac218e8022011e9be07c7a6827c5e88b236cdd
John Van Sloun <jpvansloun@gmail.com>
2024-10-01 23:01:10 -0500

    implemented initial version of basic functionality

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-grid_state = []
+grid_state = [[0 for i in range(100)] for j in range(100)]
 DEAD = 0
 LIVE = 1
 
-def initialize_grid():
-    pass
-
 def reset_grid():
-    pass
+    grid_state = [[0 for i in range(100)] for j in range(100)]
 
-def update_cell(x, y):
-    pass
+def update_cell(x, y, value):
+    grid_state[x][y] = value
 
 def print_grid():
-    pass
+    row_len = 100
+    col_len = 100
 
+    for i in range(col_len):
+        for j in range(row_len):
+            if j == row_len - 1:
+                print(grid[i][j] + "\n")
+            else:
+                print(grid[i][j])
