1d6878d2198a9bc4f10f90568a254057ffb26e75
John Van Sloun <jpvansloun@gmail.com>
2024-10-01 23:08:40 -0500

    adjusted initial functionality and added comments

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-grid_state = [[0 for i in range(100)] for j in range(100)]
+grid = [[0 for i in range(25)] for j in range(25)]
 DEAD = 0
 LIVE = 1
 
+# Resets the grid state to its initial state
 def reset_grid():
-    grid_state = [[0 for i in range(100)] for j in range(100)]
+    grid = [[0 for i in range(25)] for j in range(25)]
 
+# Updates a cell at the specified x, y index pair to value
 def update_cell(x, y, value):
-    grid_state[x][y] = value
+    grid[x][y] = value
 
+# Prints the current state of the grid to standard output
 def print_grid():
-    row_len = 100
-    col_len = 100
+    row_len = len(grid[0])
+    col_len = len(grid)
 
     for i in range(col_len):
         for j in range(row_len):
             if j == row_len - 1:
-                print(grid[i][j] + "\n")
-            else:
                 print(grid[i][j])
+            else:
+                print(grid[i][j], end="")
